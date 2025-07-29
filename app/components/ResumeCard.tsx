75da31191d635149ea9dd75575d9807a82558be5
nina <nina_nikolova@students.softuni.bg>
2025-07-29 17:34:40 +0300

    added puter

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -14,12 +14,21 @@ const ResumeCard = ({
     >
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-          <h2 className="!text-black font-bold break-words">{companyName}</h2>
-          <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+          {companyName && (
+            <h2 className="!text-black font-bold break-words">{companyName}</h2>
+          )}
+          {jobTitle && (
+            <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h2 className="!text-black font-bold">Resume</h2>
+          )}
         </div>
         <div className="flex-shrink-0">
           <ScoreCircle score={feedback.overallScore} />
         </div>
+      </div>
+      {imagePath && (
         <div className="gradient-border animate-in fade-in duration-1000">
           <div className="w-full h-full">
             <img
@@ -29,7 +38,7 @@ const ResumeCard = ({
             />
           </div>
         </div>
-      </div>
+      )}
     </Link>
   );
 };
