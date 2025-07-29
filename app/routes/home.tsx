10a0cc64d67f9266377bc94288da03a256c6e28a
nina <nina_nikolova@students.softuni.bg>
2025-07-29 15:40:28 +0300

    added navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeMaker" },
+    { name: "description", content: "Smart feedback for a choosen job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Application & Resume Rating</h1>
+
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+      {resumes.map((resume: any) => (
+        <div>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+      ))}
+    </main>
+  );
 }
